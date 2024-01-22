7546802a910faf646b1f05845b487a66ef749760
Marco <bigdatajang@gmail.com>
2024-01-23 01:24:20 +0900

    chore: prettier 설정 및 인덴트 반영

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-	extends: [
-		"@titicaca/eslint-config-triple",
-		"@titicaca/eslint-config-triple/frontend",
-		"@titicaca/eslint-config-triple/prettier",
-		"next/core-web-vitals",
-	],
+  extends: [
+    "@titicaca/eslint-config-triple",
+    "@titicaca/eslint-config-triple/frontend",
+    "@titicaca/eslint-config-triple/prettier",
+    "next/core-web-vitals",
+  ],
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,52 +1,51 @@
 import Document, {
-	Html,
-	Head,
-	Main,
-	NextScript,
-	DocumentContext,
-	DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default function MyDocument() {
-	return (
-		<Html lang='ko'>
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="ko">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
 
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-	const sheet = new ServerStyleSheet();
-	const originalRenderPage = ctx.renderPage;
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
 
-	try {
-		ctx.renderPage = () =>
-			originalRenderPage({
-				// useful for wrapping the whole react tree
-				enhanceApp: App => props =>
-					sheet.collectStyles(<App {...props} />),
-				// useful for wrapping in a per-page basis
-				enhanceComponent: Component => Component,
-			});
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // useful for wrapping the whole react tree
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        // useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      });
 
-		const initialProps: DocumentInitialProps =
-			await Document.getInitialProps(ctx);
-		return {
-			...initialProps,
-			// Styles fragment is rendered after the app and page rendering finish.
-			styles: (
-				<>
-					{initialProps.styles}
-					{sheet.getStyleElement()}
-				</>
-			),
-		};
-	} finally {
-		sheet.seal();
-	}
+    const initialProps: DocumentInitialProps =
+      await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
 };
