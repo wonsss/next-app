2dfac3cd8e987b0db670d2cda45b478ec228d00d
Marco <bigdatajang@gmail.com>
2024-01-24 00:08:22 +0900

    feat: test

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+export default function About() {
+  return <Wrapper>index</Wrapper>;
+}
+
+const Wrapper = styled.div`
+  background-color: red;
+`;
